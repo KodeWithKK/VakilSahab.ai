f4a0573025c452f4e3258bb8f5440981374fdf38
KodeWithKK <kodewithkk@gmail.com>
2025-05-25 02:06:30 +0530

    REFACTOR: temp added dynamic theme

--- frontend/contexts/theme-provider.tsx
+++ frontend/contexts/theme-provider.tsx
@@ -1,11 +1,31 @@
 "use client";
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+
+function DynamicTheme({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, [pathname]);
+
+  return <>{children}</>;
+}
 
 export default function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <DynamicTheme>{children}</DynamicTheme>
+    </NextThemesProvider>
+  );
 }
