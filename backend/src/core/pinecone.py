c8d6713f9340b3fb23b0cb2fb79a8d478636f79b
KodeWithKK <kodewithkk@gmail.com>
2025-06-25 01:11:34 +0530

    chore: remove hugging face embedding and used google generative ai embedding instead

--- backend/src/core/pinecone.py
+++ backend/src/core/pinecone.py
@@ -1,8 +1,7 @@
-from functools import lru_cache
 from typing import List
 
 from langchain.docstore.document import Document
-from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
 from src.core.config import settings
@@ -16,7 +15,7 @@ existing_indexes = pc.list_indexes().names()
 if index_name not in existing_indexes:
     pc.create_index(
         name=index_name,
-        dimension=384,
+        dimension=768,
         metric="cosine",
         spec=ServerlessSpec(cloud="aws", region="us-east-1"),
     )
@@ -24,32 +23,37 @@ if index_name not in existing_indexes:
 pinecone_index = pc.Index(index_name)
 
 # Initialize embedding model
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="models/embedding-001", google_api_key=settings.GEMINI_API_KEY
+)
 
 
-@lru_cache
-def get_embedding_model():
-    return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
-
-def build_pinecone_session(text_chunks: List[str], session_id: str):
+def build_pinecone_session(
+    text_chunks: List[str], session_id: str, batch_size: int = 100
+):
     documents = [
         Document(page_content=chunk, metadata={"session_id": session_id, "text": chunk})
         for chunk in text_chunks
     ]
 
+    print(f"Adding {len(documents)} documents to Pinecone index...")
+
     # Connect to existing Pinecone index
     vectorstore = PineconeVectorStore(
         index=pinecone_index,
-        embedding=get_embedding_model(),
+        embedding=embedding_model,
         text_key="text",  # raw text key
     )
 
-    vectorstore.add_documents(documents)
+    for i in range(0, len(documents), batch_size):
+        batch = documents[i : i + batch_size]
+        print(f"Upserting batch #{i // batch_size + 1} ({len(batch)} docs)...")
+        vectorstore.add_documents(batch)
 
 
 def load_pinecone_retriever(session_id: str, top_k: int = 4):
     vectorstore = PineconeVectorStore(
-        index=pinecone_index, embedding=get_embedding_model(), text_key="text"
+        index=pinecone_index, embedding=embedding_model, text_key="text"
     )
     retriever = vectorstore.as_retriever(
         search_kwargs={"k": top_k, "filter": {"session_id": session_id}}
